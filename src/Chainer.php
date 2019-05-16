cae5a15687a82bb25f3fb8aabc9687bcff2127af
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-05-16 03:33:55 +0200

    version: 1.2.0-rc1 which supports lumen-zero 2.0.0

--- src/Chainer.php
+++ src/Chainer.php
@@ -4,7 +4,7 @@ namespace Mathrix\Chainer;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Mathrix\Lumen\Exceptions\Http\Http400BadRequestException;
+use Mathrix\Lumen\Zero\Exceptions\Http\Http400BadRequestException;
 use Symfony\Component\HttpFoundation\ParameterBag;
 
 /**
